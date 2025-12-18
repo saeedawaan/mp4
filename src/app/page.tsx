887f3537ea1329109347a07e8e6eded1d47ff982
saeedawaan <awaanonlie@gmail.com>
2025-12-18 23:22:21 +0500

    mp4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,40 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    <div>
+      <figure className=" bg-black square p-4 dark:bg-black-800">
+        <header className='text-white text-center text-5x1 mt-1 font-bold'>MARHABA PROPERTIES</header>
+        </figure>
+        <figure className='md:flex justify-between items-center bg-blue-100 rounded-x1 p-0 md:p-1 dark bd-blue-800'>
+  <img className="w-48 h-48 rounded" src="/7.jpg" alt="mp4 flex" width="384" height="512"/>  
+    <blockquote>
+      <p className="text-center text-2x1 font-bold text-red mt-7">
+         A trusted agency for buying and selling property in Bhowana.
+      </p>
+      <p className="text-center text-2xl">
+        We deal in all kind of residential, commercial and agricultural properties in BHOWANA.
+      </p>
+      <p className='text-center text-medium font-bold'>
+        Office:Thatha Moosa, Zero Point, Bhowana.</p>
+    <p className='text-small text-center font-bold'>
+        Contact us: +92 344 45 70 707   
+         </p>
+
+    </blockquote>
+    <img className="w-48 h-48 rounded" src="/1.jpg" alt="mp4 flex" width="384" height="512"/>
+    </figure>
+    <figure className='md:flex justify-between items-center bg-blue-100 rounded-x1 p-8 md:p-1 dark bd-blue-800,'>
+    </figure>
+<figure className="md:flex justify-between items-center bg-blue-100 rounded-x1 p-8 md:p-1 dark bd-blue-800">
+</figure>
+    
+        
+        
+        
+      </div>
+
+  )
 }
+
+export default page
